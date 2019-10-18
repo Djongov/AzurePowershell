5566098eef77369110924e0442e0b3b37135c47d
Dimitar Dzhongov <49032971+Djongov@users.noreply.github.com>
2019-10-18 20:14:51 +0300

    Update and rename Delete-Empty-APS.ps1 to Delete-Empty-ASP.ps1

--- Delete-Empty-ASP.ps1
+++ Delete-Empty-ASP.ps1
@@ -1,4 +1,4 @@
-﻿
+Connect-AzAccount
 $azureSubs = Get-AzSubscription
 # The script works by querying App Serivce plans that have 0 NumberofSites property. Also because Free tier App Service plans without any sites in them are OK cost-wise, the script will not delete them. Script will delete Consumption (Y1) plans. If you want it to skip Y1 plans along with Free (F1), add another -and operator with CNotMatch "Y1"
 Write-Host "These are the App Service plans that are matching the criteria:"
