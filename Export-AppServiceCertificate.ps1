ba4e43605e639bb3d684a83e27facf539560a305
Dimitar Dzhongov <49032971+Djongov@users.noreply.github.com>
2020-01-10 15:57:59 +0200

    Add files via upload

---
+++ Export-AppServiceCertificate.ps1
@@ -0,0 +1,29 @@
+﻿#Let's define some variables
+$appServiceCertificateName = "HybridCenterSSL"
+$resourceGroupName = "App-Service-Domains"
+$keyVaultSecretName = "ac47a02f809749579b8698ebcca5d8ba"
+$subscriptionId = "dd38641d-ce43-41a5-ad40-1012fdd26a6e"
+$azureLoginEmailId = "v-didzho@microsoft.com"
+
+Connect-AzAccount
+Set-AzContext -SubscriptionId $subscriptionId
+
+$ascResource = Get-AzResource -ResourceName $appServiceCertificateName -ResourceGroupName $resourceGroupName -ResourceType "Microsoft.CertificateRegistration/certificateOrders" -ApiVersion "2015-08-01"
+
+$certificateProperties=Get-Member -InputObject $ascResource.Properties.certificates[0] -MemberType NoteProperty
+$certificateName = $certificateProperties[0].Name
+$keyVaultId = $ascResource.Properties.certificates[0].$certificateName.KeyVaultId
+$keyVaultSecretName = $ascResource.Properties.certificates[0].$certificateName.KeyVaultSecretName
+$keyVaultIdParts = $keyVaultId.Split("/")
+$keyVaultName = $keyVaultIdParts[$keyVaultIdParts.Length - 1]
+$keyVaultResourceGroupName = $keyVaultIdParts[$keyVaultIdParts.Length - 5]
+Set-AzKeyVaultAccessPolicy -ResourceGroupName $keyVaultResourceGroupName -VaultName $keyVaultName -UserPrincipalName $azureLoginEmailId -PermissionsToSecrets get
+$secret = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $keyVaultSecretName
+$pfxCertObject=New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList @([Convert]::FromBase64String($secret.SecretValueText),"", [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
+$pfxPassword = -join ((65..90) + (97..122) + (48..57) | Get-Random -Count 50 | % {[char]$_})
+$currentDirectory = (Get-Location -PSProvider FileSystem).ProviderPath
+[Environment]::CurrentDirectory = (Get-Location -PSProvider FileSystem).ProviderPath
+[io.file]::WriteAllBytes(".\appservicecertificate.pfx", $pfxCertObject.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12, $pfxPassword))
+Write-Host "Created an App Service Certificate copy at: $currentDirectory\appservicecertificate.pfx"
+Write-Warning "For security reasons, do not store the PFX password. Use it directly from the console as required."
+Write-Host "PFX password: $pfxPassword"
